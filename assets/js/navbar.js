5ce0d9164d20626a2e981610083e66d9dbde8b6f
Pedro <pedrocunha.furtado@gmail.com>
2024-09-16 17:31:45 -0300

    Adição de Hamburguer, configuração de navbar e alinhamento na responsividade em diferentes tamanhos

---
+++ assets/js/navbar.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navbarUl = document.querySelector(".navbar ul");
+  
+    menuToggle.addEventListener("click", function () {
+      navbarUl.classList.toggle("active");
+    });
+  });
+  
