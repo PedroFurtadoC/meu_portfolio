49c959c5d37dd1d6bf63f28f3042c1c394ff2976
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-05-21 20:18:57 -0300

    Remoção do antigo portfolio

--- assets/js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.querySelector(".menu-toggle");
-  const navbarUl = document.querySelector(".navbar ul");
-
-  menuToggle.addEventListener("click", function () {
-    navbarUl.classList.toggle("active");
-  });
-});
