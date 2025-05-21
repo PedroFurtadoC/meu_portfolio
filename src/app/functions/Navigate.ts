4b9da2bab212e922c51302fde8a0d433bbc7b078
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-05-21 20:11:51 -0300

    Teste do calendly

--- src/app/functions/Navigate.ts
+++
@@ -1,7 +0,0 @@
-export default function navigate(selector: string) {
-    const link = document.createElement('a');
-    link.href = selector;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
--- src/app/functions/NewTab.ts
+++
@@ -1,9 +0,0 @@
-export default function openNewTab(url: string) {
-    const link = document.createElement('a');
-    link.href = url;
-    link.target = '_blank';
-    link.rel = 'noopener noreferrer'
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
