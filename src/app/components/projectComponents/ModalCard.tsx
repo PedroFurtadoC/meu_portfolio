6b4a4b2286d0988d29232626d0cc79625e825998
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-06-20 19:42:09 -0300

    Novo layout e possibilidade de layouts customizados

--- src/app/components/projectComponents/ModalCard.tsx
+++ src/app/components/projectComponents/ModalCard.tsx
@@ -1,13 +1,10 @@
 import { PContent } from "@/app/content/projects-content";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import SimpleBar from "simplebar-react";
 import "simplebar-react/dist/simplebar.min.css";
 import FontelloIcon from "../shared/FontelloIcon";
-import Image from "next/image";
 import Portal from "../shared/Portal";
-import openNewTab from "@/app/functions/NewTab";
-import { YouTubeEmbed } from "@next/third-parties/google";
-import useWindowResize from "@/app/hooks/useWindowResize";
+import { Layout1, Layout2, Layout3 } from "./Layouts";
 
 interface Props {
 	isVisible: boolean;
@@ -28,6 +25,13 @@ export default function ModalCard({ isVisible, onClose, content }: Props) {
 				return <Layout1 content={content} />;
 			case 2:
 				return <Layout2 content={content} />;
+			case 3:
+				return <Layout3 content={content} />;
+			case "custom":
+				// Verifica se existe um CustomLayout e renderiza com as props
+				return content.CustomLayout ? (
+					<content.CustomLayout content={content} />
+				) : null;
 		}
 	};
 
@@ -56,165 +60,3 @@ export default function ModalCard({ isVisible, onClose, content }: Props) {
 		</Portal>
 	);
 }
-
-const Layout1 = ({ content }: { content: PContent }) => {
-	const windowRef = useWindowResize();
-	const [width, setWidth] = useState(700);
-	useEffect(() => {
-		if (windowRef.width >= 900) {
-			setWidth(700);
-		} else if (windowRef.width >= 700) {
-			setWidth(500);
-		} else {
-			setWidth(300);
-		}
-	}, [windowRef]);
-
-	return (
-		<>
-			<h1 className="self-center font-bold text-3xl md:text-5xl text-accent">
-				{content.title}
-			</h1>
-			<div className="flex flex-col pt-10 mb-15">
-				<p className="text-lg md:text-2xl">{content.text}</p>
-				{content.optionalText && (
-					<p className="text-lg md:text-2xl mt-5">
-						{content.optionalText}
-					</p>
-				)}
-				{content.optionalText2 && (
-					<p className="text-lg md:text-2xl mt-5">
-						{content.optionalText2}
-					</p>
-				)}
-			</div>
-			<div className="flex flex-row flex-wrap justify-around gap-10 mt-10">
-				<Image
-					className="h-auto w-[300px] rounded-xl shadow-lg shadow-black transition-all hover:scale-105"
-					src={content.image}
-					alt={"Imagem " + content.title}
-					loading="lazy"
-					aria-hidden
-					unoptimized
-				/>
-				{content.optionalImage && (
-					<Image
-						className="h-auto w-[300px] rounded-xl shadow-lg shadow-black transition-all hover:scale-105"
-						src={content.optionalImage}
-						alt={"Imagem " + content.title}
-						loading="lazy"
-						aria-hidden
-						unoptimized
-					/>
-				)}
-				{content.optionalImage2 && (
-					<Image
-						className="h-auto w-[300px] rounded-xl shadow-lg shadow-black transition-all hover:scale-105"
-						src={content.optionalImage2}
-						alt={"Imagem " + content.title}
-						loading="lazy"
-						aria-hidden
-						unoptimized
-					/>
-				)}
-			</div>
-			{content.link && (
-				<p
-					className="my-10 text-lg md:text-2xl font-bold text-extra-lilac cursor-pointer"
-					role="button"
-					onClick={() => openNewTab(content.link?.url!)}
-				>
-					{content.link.description ?? content.link.url}
-				</p>
-			)}
-			{content.ytVideoId && (
-				<div className="flex flex-col w-full mb-10 pb-10 items-center">
-					<YouTubeEmbed videoid={content.ytVideoId} width={width} />
-				</div>
-			)}
-		</>
-	);
-};
-
-const Layout2 = ({ content }: { content: PContent }) => {
-	const windowRef = useWindowResize();
-	const [width, setWidth] = useState(700);
-	useEffect(() => {
-		if (windowRef.width >= 900) {
-			setWidth(700);
-		} else if (windowRef.width >= 700) {
-			setWidth(500);
-		} else {
-			setWidth(300);
-		}
-	}, [windowRef]);
-
-	return (
-		<>
-			<h1 className="self-center font-bold text-3xl md:text-5xl text-accent">
-				{content.title}
-			</h1>
-			<div className="flex flex-col gap-[50px] pt-10 mb-15">
-				<div className="flex flex-col md:flex-row justify-center gap-[50px]">
-					<p className="text-lg md:text-2xl">{content.text}</p>
-					<Image
-						className="h-auto w-[300px] md:w-[250px] xl:w-[500px] aspect-video rounded-xl shadow-lg shadow-black transition-all hover:scale-105"
-						src={content.image}
-						alt={"Imagem " + content.title}
-						loading="lazy"
-						aria-hidden
-						unoptimized
-					/>
-				</div>
-				<div className="flex flex-col md:flex-row-reverse justify-center gap-[50px]">
-					{content.optionalText && (
-						<p className="text-lg md:text-2xl mt-5">
-							{content.optionalText}
-						</p>
-					)}
-					{content.optionalImage && (
-						<Image
-							className="h-auto w-[300px] md:w-[250px] xl:w-[500px] aspect-video rounded-xl shadow-lg shadow-black transition-all hover:scale-105"
-							src={content.optionalImage}
-							alt={"Imagem " + content.title}
-							loading="lazy"
-							aria-hidden
-							unoptimized
-						/>
-					)}
-				</div>
-				<div className="flex flex-col md:flex-row justify-center gap-[50px]">
-					{content.optionalText && (
-						<p className="text-lg md:text-2xl mt-5">
-							{content.optionalText2}
-						</p>
-					)}
-					{content.optionalImage && (
-						<Image
-							className="h-auto w-[300px] md:w-[250px] xl:w-[500px] aspect-video rounded-xl shadow-lg shadow-black transition-all hover:scale-105"
-							src={content.optionalImage2}
-							alt={"Imagem " + content.title}
-							loading="lazy"
-							aria-hidden
-							unoptimized
-						/>
-					)}
-				</div>
-			</div>
-			{content.link && (
-				<p
-					className="my-10 text-lg md:text-2xl font-bold text-extra-lilac cursor-pointer"
-					role="button"
-					onClick={() => openNewTab(content.link?.url!)}
-				>
-					{content.link.description ?? content.link.url}
-				</p>
-			)}
-			{content.ytVideoId && (
-				<div className="flex flex-col w-full mb-10 pb-10 items-center">
-					<YouTubeEmbed videoid={content.ytVideoId} width={width} />
-				</div>
-			)}
-		</>
-	);
-};
--- src/app/content/projects-content.ts
+++ src/app/content/projects-content.ts
@@ -1,3 +1,4 @@
+import { CustomLayoutExample } from "../components/projectComponents/Layouts";
 import { Color } from "../types/constants";
 
 export interface Project {
@@ -9,7 +10,7 @@ export interface Project {
 }
 
 export interface PContent {
-	layout: 1 | 2;
+	layout: 1 | 2 | 3 | "custom";
 	title: string;
 	text: string;
 	optionalText?: string;
@@ -19,11 +20,17 @@ export interface PContent {
 	image: string;
 	optionalImage: string;
 	optionalImage2: string;
+	/* ID do vídeo do Youtube (aquele código no final do link) */
 	ytVideoId?: string;
+	/* Componente customizado, deve ser definido como "custom" para utilizar
+	o componente customizado. Ele recebe os props de acordo com PContent, vide 
+	src/app/projectComponentes/Layouts.tsx para mais detalhes de como implementar  */
+	CustomLayout?: React.ComponentType<{ content: PContent }>;
 }
 
 export interface PLink {
 	url: string;
+	/* Se não houver descrição, aparecerá apenas o link */
 	description?: string;
 }
 
@@ -71,6 +78,53 @@ export const projectContent: Project[] = [
 				url: "https:www.google.com.br",
 				description: "Teste de link",
 			},
+		},
+	},
+	{
+		title: "Placeholder 3",
+		shortDescription:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex.",
+		picture: "image.png",
+		bgColor: "#ececec",
+		modalContent: {
+			layout: 3,
+			title: "Layout 3",
+			text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex.",
+			optionalText:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex.",
+
+			image: require("../assets/image.png"),
+			optionalImage: require("../assets/image.png"),
+			optionalImage2: require("../assets/image.png"),
+			ytVideoId: "jNQXAC9IVRw",
+			link: {
+				url: "https:www.google.com.br",
+				description: "Teste de link",
+			},
+		},
+	},
+	{
+		title: "Placeholder 4",
+		shortDescription:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex.",
+		picture: "image.png",
+		bgColor: "#ececec",
+		modalContent: {
+			layout: "custom",
+			title: "Custom Layout",
+			text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex.",
+			optionalText:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis faucibus mi, vel dapibus ex.",
+
+			image: require("../assets/image.png"),
+			optionalImage: require("../assets/image.png"),
+			optionalImage2: require("../assets/image.png"),
+			ytVideoId: "jNQXAC9IVRw",
+			link: {
+				url: "https:www.google.com.br",
+				description: "Teste de link",
+			},
+			CustomLayout: CustomLayoutExample,
 		},
 	},
 ];
