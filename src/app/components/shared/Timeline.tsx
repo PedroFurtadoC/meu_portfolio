16c1594c3052258b72d748402557273f9065a918
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-06-25 19:08:16 -0300

    Timeline com novo layout

--- src/app/components/shared/Timeline.tsx
+++ src/app/components/shared/Timeline.tsx
@@ -35,12 +35,12 @@ const TimelineCard = ({ item, index, length }: TimelineCardProps) => {
 	const width = windowWidth * 0.9;
 	const maxWidth = windowWidth > 1200 ? "750px" : "600px";
 
-	const renderTimeLineDivision = (index: number) => {
+	const renderStartDivision = (index: number) => {
 		if (index < length - 1) {
-			if (windowWidth < 1000)
+			/* if (windowWidth < 1000)
 				return (
-					<hr className="h-[150px] w-[1px] border-l-[7px] border-l-accent border-dashed" />
-				);
+					<hr className="h-[150px] w-[1px] border-l-[7px] border-l-secondary border-dashed" />
+				); */
 
 			return (
 				<div
@@ -48,68 +48,91 @@ const TimelineCard = ({ item, index, length }: TimelineCardProps) => {
 						height: `${cardHeight}px`,
 						flexDirection: index % 2 === 0 ? "row" : "row-reverse",
 					}}
-					className="relative flex items-center flex-grow"
+					className="relative flex items-center w-[10%] xl:w-1/5"
 				>
-					<hr className="w-[40%] h-[1px] border-0 border-b-[5px] border-b-accent border-dashed" />
+					<hr className="w-1/3 h-[1px] border-0 border-b-[5px] border-b-secondary border-dashed" />
 					<hr
-						className="w-[1px] border-0 border-l-[5px] border-l-accent border-dashed"
+						className="w-[1px] border-0 border-l-[5px] border-l-secondary border-dashed"
 						style={{
-							height: `${cardHeight}px`,
-							transform: `translateY(${cardHeight / 2 - 2}px)`,
+							height: `${cardHeight * 0.49 + 80}px`,
+							transform: `translateY(49.5%)`,
 						}}
 					/>
 				</div>
 			);
 		} else {
-			return null;
+			return <div className="w-[10%] xl:w-1/5" />;
+		}
+	};
+
+	const renderEndDivision = (index: number) => {
+		if (index === 0) {
+			return <div className="w-[10%] xl:w-1/5" />;
+		} else {
+			return (
+				<div
+					style={{
+						height: `${cardHeight}px`,
+						flexDirection: index % 2 !== 0 ? "row" : "row-reverse",
+					}}
+					className="relative flex items-center w-[10%] xl:w-1/5"
+				>
+					<hr className="w-1/3 h-[1px] border-0 border-b-[5px] border-b-secondary border-dashed" />
+					<hr
+						className="w-[1px] border-0 border-l-[5px] border-l-secondary border-dashed"
+						style={{
+							height: `${cardHeight * 0.5 + 80}px`,
+							transform: `translateY(-49.5%)`,
+						}}
+					/>
+				</div>
+			);
 		}
 	};
 
 	return (
 		<div
-			className="flex items-center"
+			className="flex items-center w-full"
 			style={{
-				marginBottom:
-					windowWidth > 1000
-						? index === length - 1
-							? "0px"
-							: `${cardHeight / 2}px`
-						: "0px",
-				flexDirection:
-					windowWidth > 1000
-						? index % 2 === 0
-							? "row"
-							: "row-reverse"
-						: "column",
+				marginBottom: length === 1 ? "0px" : "80px",
+				flexDirection: index % 2 === 0 ? "row" : "row-reverse",
 			}}
 		>
+			{renderEndDivision(index)}
 			<div
 				ref={cardRef}
-				style={{
-					width: `${width}px`,
-					maxWidth: maxWidth,
-				}}
-				className={`min-h-[200px] bg-nav rounded-xl p-8 flex flex-col text-white font-bold card z-10`}
+				className={`min-h-[200px] w-[80%] xl:w-3/5 bg-primary rounded-xl py-8 px-10 md:px-20 flex flex-col text-white font-bold z-10`}
 			>
-				<h2
-					className={`${
-						index % 2 === 0
-							? "text-extra-lilac"
-							: "text-extra-green"
-					} text-2xl md:text-3xl mb-4`}
-				>
-					{item.title}
-				</h2>
+				<div className="flex flex-col lg:flex-row justify-between lg:items-center mb-6 gap-5">
+					<h2 className="text-accent text-3xl md:text-4xl max-w-[100%] lg:max-w-[50%]">
+						{item.title}
+					</h2>
+					<h3 className="text-sm md:text-base font-extralight">
+						{item.start.toUpperCase()} | {item.end.toUpperCase()}
+					</h3>
+				</div>
 				<p className="text-foreground font-normal text-xl md:text-2xl mb-4">
 					{item.text}
 				</p>
-				{item.optionalText && (
-					<p className="text-foreground font-normal text-xl md:text-2xl">
-						{item.optionalText}
-					</p>
+				{item.habilities && (
+					<>
+						<p className="text-foreground font-normal text-xl md:text-2xl mt-10 mb-4">
+							Habilidades específicas
+						</p>
+						<ul className="font-light font-inter list-disc list-inside text-xl md:text-2xl columns-2">
+							{item.habilities.map((hability) => (
+								<li
+									className="mb-2"
+									key={item.title + " " + hability}
+								>
+									{hability}
+								</li>
+							))}
+						</ul>
+					</>
 				)}
 			</div>
-			{renderTimeLineDivision(index)}
+			{renderStartDivision(index)}
 		</div>
 	);
 };
--- src/app/content/timeline-content.ts
+++ src/app/content/timeline-content.ts
@@ -1,32 +1,40 @@
 export interface TimelineContent {
 	title: string;
+	start: string;
+	end: string;
 	text: string;
-	optionalText?: string;
+	habilities?: string[];
 }
 
 //IMPORTANTE: o "title" é utilizado como key, não podem haver repetições
 
 export const timelineContent: TimelineContent[] = [
 	{
-		title: "Texto 1",
-		text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit",
+		title: "Dimastec",
+		start: "fevereiro 2022",
+		end: "dezembro 2023",
+		text: "Estagiário responsável pela área de apresentações técnicas aliadas ao suporte de usuário, na Dimastec fui braço direito da equipe de vendas para apresentar o DT Faceum de forma técnica, clara, vendável e inteligente.",
+		habilities: ["Teste", "Teste2", "Teste3", "Teste4"],
 	},
 	{
-		title: "Texto 2",
-		text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit",
-		optionalText:
-			"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+		title: "Consultor de Tecnologia",
+		start: "fevereiro 2022",
+		end: "dezembro 2023",
+		text: "Consultoria em tecnologia, resolvendo problemas de forma alinhada ao que usuário procura, sempre humanizado, simplificado, claro, estudado e trazendo as melhores soluções.",
+		habilities: ["Teste", "Teste2", "Teste3", "Teste4"],
 	},
 	{
-		title: "Texto 3",
-		text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit",
-		optionalText:
-			"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+		title: "TI na Agência 6P",
+		start: "fevereiro 2022",
+		end: "dezembro 2023",
+		text: "Na agência 6P fui estagiário e responsável por cuidar de toda a área de TI da empresa, resolvendo problemas, organizando processos, realizando suporte ao usuário rápido, funcional e empático.",
+		habilities: ["Teste", "Teste2", "Teste3", "Teste4"],
 	},
 	{
-		title: "Texto 4",
-		text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit",
-		optionalText:
-			"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+		title: "Zapit",
+		start: "fevereiro 2022",
+		end: "dezembro 2023",
+		text: "Sócio na Zapit, empresa de vending machines focada em experiência do cliente e trazendo soluções com máquinas da melhor forma possível, segura, organizada, limpa, rápida e funcional.",
+		habilities: ["Teste", "Teste2", "Teste3", "Teste4"],
 	},
 ];
