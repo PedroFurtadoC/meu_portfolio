261648f142391486ed7d87ed8fb153df6142db16
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-07-04 15:08:15 -0300

    Teste do basepath para deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "",
-  output: "export",
-  reactStrictMode: true,
+	basePath: "/meu_portfolio",
+	output: "export",
+	reactStrictMode: true,
 };
+
+/* nextConfig.basePath =
+	process.env.NODE_ENV === "production" ? "/meu_portfolio" : ""; */
 
 export default nextConfig;
