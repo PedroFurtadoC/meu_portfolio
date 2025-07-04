92dd9558a1609e1640990c161bd9cc129bc25f18
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-07-04 15:23:36 -0300

    BasePath dinâmico

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	basePath: "/meu_portfolio",
 	output: "export",
 	reactStrictMode: true,
 };
 
-/* nextConfig.basePath =
-	process.env.NODE_ENV === "production" ? "/meu_portfolio" : ""; */
+nextConfig.basePath =
+	process.env.NODE_ENV === "production" ? "/meu_portfolio" : "";
 
 export default nextConfig;
