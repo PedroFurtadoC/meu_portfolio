c00bb03e107f08ccc24463aef9413572c3ea714a
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-07-04 14:57:21 -0300

    Limpeza

--- src/app/types/constants.d.ts
+++
@@ -1,7 +0,0 @@
-/* COLOR TYPE */
-
-type RGB = `rgb(${number}, ${number}, ${number})`;
-type RGBA = `rgba(${number}, ${number}, ${number}, ${number})`;
-type HEX = `#${string}`;
-
-export type Color = RGB | RGBA | HEX;
