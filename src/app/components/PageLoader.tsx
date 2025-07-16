df57b1c70263c673609abc678960206b107601e3
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-07-16 19:27:06 -0300

    Desmontar loader após concluir carregamento

--- src/app/components/PageLoader.tsx
+++ src/app/components/PageLoader.tsx
@@ -1,22 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 interface Props {
 	isLoading: boolean;
 }
 
 const PageLoader = ({ isLoading }: Props) => {
+	const [shouldRender, setShouldRender] = useState(isLoading);
+
 	useEffect(() => {
+		let timeout: NodeJS.Timeout;
+
 		if (isLoading) {
+			setShouldRender(true);
 			document.body.style.overflowY = "hidden";
 		} else {
 			document.body.style.overflowY = "auto";
+			timeout = setTimeout(() => {
+				setShouldRender(false);
+			}, 1100);
 		}
+
+		return () => clearTimeout(timeout);
 	}, [isLoading]);
 
 	const text = "CARREGANDO CONTEÚDO...";
 
+	if (!shouldRender) return null;
+
 	return (
-		<div style={{ opacity: isLoading ? 1 : 0 }} className="loading-screen">
+		<div
+			style={{
+				opacity: isLoading ? 1 : 0,
+				pointerEvents: isLoading ? "all" : "none",
+			}}
+			className="loading-screen"
+		>
 			{isLoading && (
 				<div className="flex flex-col items-center gap-10">
 					<div className="loader" />
